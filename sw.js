390f4b84b04518ce5b1fc037d7e3cb53cae0bf68
directionary-guy <designasaur@gmail.com>
2026-08-23 13:19:45 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -16,7 +16,7 @@
    index.html.
    ============================================================================ */
 
-const CACHE = 'unimpossible-v15';
+const CACHE = 'unimpossible-v16';
 
 const ASSETS = [
   './',
